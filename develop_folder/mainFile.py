c2390b8697c7cfcf404aaefef74b36f11c2ca24c
lolutorch <lolutorch>
2023-05-11 05:59:53 +0000

    i am proud
    New banch

--- develop_folder/mainFile.py
+++ develop_folder/mainFile.py
@@ -7,3 +7,14 @@ print(f"hello,", develop )
 se = int(input("give a number: "))
 de = se *1000
 print(f"Your annual salary is {de} dollars + Benefits")
+
+# COMMAND ----------
+
+update = "my new branch saxxbranch is created"
+ss = "I am proud, "
+print(f" {ss} do you know why? bcz {update}")
+
+
+# COMMAND ----------
+
+
